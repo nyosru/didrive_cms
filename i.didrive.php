d74085d1a26e42c92b2376cd450c5bd92711ef2b
Сергей <nyos@rambler.ru>
2020-08-16 00:22:56 +0500

    0.0.11

--- i.didrive.php
+++ i.didrive.php
@@ -12,9 +12,9 @@ if ($_SERVER['HTTP_HOST'] == 'photo.uralweb.info' || $_SERVER['HTTP_HOST'] == 'a
 }
 
 if (
-    strpos($_SERVER['HTTP_HOST'], 'dev.') !== false ||
-    $_SERVER['HTTP_HOST'] == 'yapdomik.uralweb.info' ||
-    $_SERVER['HTTP_HOST'] == 'adomik.uralweb.info'
+        strpos($_SERVER['HTTP_HOST'], 'dev.') !== false ||
+        $_SERVER['HTTP_HOST'] == 'yapdomik.uralweb.info' ||
+        $_SERVER['HTTP_HOST'] == 'adomik.uralweb.info'
 ) {
     ini_set('error_reporting', E_ALL);
     ini_set('display_errors', 1);
@@ -50,12 +50,12 @@ try {
         'rand' => rand(1000, 9999)
     );
 
-    // \f\timer_start(78);
-    
+// \f\timer_start(78);
+
     require_once($_SERVER['DOCUMENT_ROOT'] . '/all/0.start.php');
-    
-    // \f\pa(\f\timer_stop(78));
-    
+
+// \f\pa(\f\timer_stop(78));
+
 
     $vv['db'] = $db;
 
@@ -88,84 +88,102 @@ try {
      */
     if (!isset($_SESSION['now_user_di']['id'])) {
 
-        // если есть картинка для входа ... показываем её
-        if( file_exists(DR.dir_site_sd.'didrive_enter_img.jpg') )
-        $vv['didrive_enter_img'] = dir_site_sd.'didrive_enter_img.jpg';
+// если есть картинка для входа ... показываем её
+        if (file_exists(DR . dir_site_sd . 'didrive_enter_img.jpg'))
+            $vv['didrive_enter_img'] = dir_site_sd . 'didrive_enter_img.jpg';
 
         $vv['id_app'] = 7171647; //Айди приложения
         $vv['url_script'] = 'https://' . $_SERVER['HTTP_HOST'] . '/i.didrive.php'; //ссылка на скрипт auth_vk.php
         $vv['vk_api_url'] = '<a href="https://oauth.vk.com/authorize?client_id=' . $vv['id_app'] . '&redirect_uri=' . $vv['url_script'] . '&response_type=code" >Войти через ВК</a></p>';
 
+
 // после клика по ссылке "войти через вк" отправили запрос и нам пришёл код .. из которого надо достать данные (используем id)
 
-        if (!empty($_GET['code'])) {
+        if (empty($_REQUEST['level']) && !empty($_REQUEST['code'])) {
 
             $id_app = $vv['id_app']; //Айди приложения
             $secret_app = 'srJxX0eTaPnGIEnTcCfJ'; // Защищённый ключ. Можно узнать там же где и айди
+
             $url_script = $vv['url_script']; //ссылка на этот скрипт
-            $token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id=' . $id_app . '&client_secret=' . $secret_app . '&code=' . $_GET['code'] . '&redirect_uri=' . $url_script), true);
+
+            $t = 'https://oauth.vk.com/access_token?client_id=' . $id_app . '&client_secret=' . $secret_app . '&code=' . $_GET['code'] . '&redirect_uri=' . $url_script;
+            $token = json_decode( \f\get_curl_https_uri($t) , true);
+
+            if (isset($token['error'])) {
+                header('Location: /i.didrive.php?warn=произошла ошибка ВК: ' . $token['error']);
+            }
+
             $fields = 'first_name,last_name,photo_200_orig';
 
-            // $fields = 'photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, domain, has_mobile, contacts, site, education, universities, schools, status, last_seen, followers_count, common_count, occupation, nickname, relatives, relation, personal, connections, exports, activities, interests, music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts, can_see_audio, can_write_private_message, can_send_friend_request, is_favorite, is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend, friend_status, career, military, blacklisted, blacklisted_by_me, can_be_invited_group';
-            // $fields = 'photo_id, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200, contacts, status, followers_count, common_count, occupation, nickname, timezone';
-
             $uinf = json_decode(file_get_contents('https://api.vk.com/method/users.get?uids=' . $token['user_id'] . '&fields=' . $fields . '&access_token=' . $token['access_token'] . '&v=5.80'), true);
-
-            // \f\pa($uinf);
-//            $_SESSION['name'] = $uinf['response'][0]['first_name'];
-//            $_SESSION['name_family'] = $uinf['response'][0]['last_name'];
-//            $_SESSION['uid'] = $token['user_id'];
-//            $_SESSION['access_token'] = $token['access_token'];
 
             \Nyos\mod\Lk::$type = 'now_user_di';
 
             if (!empty($uinf['response'][0]['id'])) {
-                $_SESSION[\Nyos\mod\Lk::$type] = \Nyos\Mod\Lk::enter($db, $uinf['response'][0]['id']);
+
+                $_SESSION[\Nyos\mod\Lk::$type] = \Nyos\Mod\Lk::enterVk($db, $uinf['response'][0]['id']);
+
+                $folder = \Nyos\Nyos::$folder_now . ( strpos($_SERVER['PHP_SELF'], 'didrive') !== false ? '_di' : '' );
+
+                $sql = 'INSERT INTO `gm_user` ( `folder`, `name`, `family`, `avatar`, `soc_web`, `soc_web_link`, `soc_web_id` ) '
+                        . ' VALUES ( :folder , :name , :family , :avatar , :soc_web , :soc_web_link , :soc_web_id ); ';
+                $ff = $db->prepare($sql);
+                $ff->execute([
+                    ':folder' => $folder,
+                    ':name' => $uinf['response'][0]['first_name'],
+                    ':family' => $uinf['response'][0]['last_name'],
+                    ':avatar' => $uinf['response'][0]['photo_200_orig'],
+                    ':soc_web' => 'vk',
+                    ':soc_web_link' => 'https://vk.com/id' . $uinf['response'][0]['id'],
+                    ':soc_web_id' => $uinf['response'][0]['id']
+                ]);
+
+                $enter = \Nyos\Mod\Lk::enterVk($db, $uinf['response'][0]['id']);
 
                 \nyos\Msg::sendTelegramm('Вход в управление с ВК' . PHP_EOL
                         . implode('+', $uinf['response'][0])
                         , null, 2);
 
-                // если это я
+// если это я
                 if ($uinf['response'][0]['id'] == '5903492')
                     $_SESSION[\Nyos\mod\Lk::$type]['access'] = 'admin';
-            }
 
-            // die();
-            header("Location: /i.didrive.php");
+                header("Location: /i.didrive.php");
+            } else {
+                header("Location: /i.didrive.php?warn=что то пошло ен так, повторите и обратитесь в тех. поддержку");
+            }
         }
 
 
 // авторизация через вк
         if (!empty($_REQUEST['uid']) && !empty($_REQUEST['hash'])) {
 
-            \f\pa($_REQUEST, '', '', 'request');
-
+            // \f\pa($_REQUEST, '', '', 'request');
 // проверка хеша при авторизации в вк
             if (1 == 1) {
                 $check_hash = false;
-                // приложуха adommik
+// приложуха adommik
                 $ap_id = 7171647;
                 $secret_key = 'srJxX0eTaPnGIEnTcCfJ';
                 if ($_REQUEST['hash'] == md5(( $ap_id ?? '' ) . $_REQUEST['uid'] . ( $secret_key ?? '' )))
                     $check_hash = true;
             }
 
-            // если хеш норм то проходим авторизацию
+// если хеш норм то проходим авторизацию
             if (isset($check_hash) && $check_hash === true) {
 
-                // \f\pa($_REQUEST);
-                // \f\pa($_SESSION);
+// \f\pa($_REQUEST);
+// \f\pa($_SESSION);
 
                 if (!class_exists('Nyos\\mod\\Lk')) {
-                    //throw new \NyosEx('Не обнаружен класс lk');
+//throw new \NyosEx('Не обнаружен класс lk');
                     require_once DR . '/vendor/didrive_mod/lk/class.php';
                 }
 
                 \Nyos\mod\Lk::$type = 'now_user_di';
                 $_SESSION[\Nyos\mod\Lk::$type] = \Nyos\Mod\Lk::enter($db, $_REQUEST['uid']);
 
-                // если это я
+// если это я
                 if (!empty($_REQUEST['uid']) && $_REQUEST['uid'] == '5903492')
                     $_SESSION[\Nyos\mod\Lk::$type]['access'] = 'admin';
 
@@ -258,7 +276,7 @@ try {
 
             if (!class_exists('Nyos\\mod\\Lk')) {
 
-                //throw new \NyosEx('Не обнаружен класс lk');
+//throw new \NyosEx('Не обнаружен класс lk');
                 require_once DR . '/vendor/didrive_mod/lk/class.php';
             }
 
@@ -417,31 +435,31 @@ try {
         } catch (\Error $ex) {
 //\f\redirect('/', 'i.didrive.php', array('rand' => rand(0, 100), 'warn' => $ex->getMessage() ));
             $vv['warn'] = 'Произошла ошибка <pre> '
-                . PHP_EOL . $ex->getMessage() . ' #' . $ex->getCode()
-                . PHP_EOL . $ex->getFile() . ' #' . $ex->getLine()
-                . PHP_EOL . $ex->getTraceAsString() . '</pre>';
+                    . PHP_EOL . $ex->getMessage() . ' #' . $ex->getCode()
+                    . PHP_EOL . $ex->getFile() . ' #' . $ex->getLine()
+                    . PHP_EOL . $ex->getTraceAsString() . '</pre>';
         }
     }
 
-    
-    
-    
+
+
+
     if (file_exists(DR . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js')) {
-        // $vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
-        if( empty( $vv['in_body_end'] ) ){
-        $vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
-        }else{
-        array_unshift( $vv['in_body_end'], '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>');
+// $vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
+        if (empty($vv['in_body_end'])) {
+            $vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
+        } else {
+            array_unshift($vv['in_body_end'], '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>');
         }
     }
 
     if (file_exists(DR . DS . 'didrive' . DS . 'js.js')) {
-        // $vv['in_body_end'][] = '<script src="/didrive/js.js"></script>';
+// $vv['in_body_end'][] = '<script src="/didrive/js.js"></script>';
         array_unshift($vv['in_body_end'], '<script src="/didrive/js.js"></script>');
     }
 
-    //$vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
-    // \f\pa( $vv['in_body_end'] );
+//$vv['in_body_end'][] = '<script src="' . DS . 'vendor' . DS . 'didrive' . DS . 'base' . DS . 'js.js"></script>';
+// \f\pa( $vv['in_body_end'] );
 
     /**
      * обработка шаблона и вывод
@@ -518,7 +536,7 @@ try {
     require DR . '/all/twig.function.php';
 
     $vv['a_menu'] = \Nyos\Nyos::$a_menu;
-    //\f\pa($vv['a_menu']);
+//\f\pa($vv['a_menu']);
 
 
     $twig->addGlobal('session', $_SESSION);
@@ -541,9 +559,9 @@ try {
     $ttwig = $twig->loadTemplate($tpl_print_end ?? 'didrive/tpl/didrive.htm');
 
 
-    // \f\timer_start(331);
+// \f\timer_start(331);
     echo $ttwig->render($vv);
-    // \f\pa( 'печать в твиг '.\f\timer_stop(331));
+// \f\pa( 'печать в твиг '.\f\timer_stop(331));
 //            if ($_SERVER['HTTP_HOST'] == 'adomik.uralweb.info') {
 //                // $sm = 0;
 //                $sm2 = 0;
@@ -560,10 +578,7 @@ try {
     ob_end_clean();
 
 
-    // \f\timer_start(781);
-
-
-
+// \f\timer_start(781);
 //            if ($_SERVER['HTTP_HOST'] == 'adomik.uralweb.info') {
 //                // $sm = 0;
 //                $sm2 = 0;
@@ -577,7 +592,7 @@ try {
 
     $r22 = $r23 = array();
 
-    // \f\pa($vv['in_body_end'],'','','in_body_end');
+// \f\pa($vv['in_body_end'],'','','in_body_end');
 //                $r22 = $r23 = array();
 
     $body_end_str = $body_start_str = '';
@@ -610,16 +625,15 @@ try {
     $r22[] = ' =memory_usage= ';
     $r23[] = round($startMemory / 1024 / 1024, 2);
 // echo '<br/>xxx'.__LINE__.' - '.round($startMemory/1024/1024,2);
-    // \f\timer_start(33);
+// \f\timer_start(33);
 
     if (!empty($r22))
         $r = str_replace($r22, $r23, $r);
 
-    // \f\pa( 'замена в body '.\f\timer_stop(33));
+// \f\pa( 'замена в body '.\f\timer_stop(33));
+// \f\pa(\f\timer_stop(781));
 
-    // \f\pa(\f\timer_stop(781));
 
-    
     die($r);
 
 //    require_once( $_SERVER['DOCUMENT_ROOT'] . '/all/inf.post.php' );
@@ -658,7 +672,8 @@ try {
         \nyos\Msg::sendTelegramm($text, null, 1);
 
     die(str_replace('{text}', $text1, file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/template/body_error.htm')));
-} catch (\Exception $ex) {
+} catch (\PDOException $ex) {
+
 
     $text1 = '<pre>--- ' . __FILE__ . ' ' . __LINE__ . '-------'
             . PHP_EOL . $ex->getMessage() . ' #' . $ex->getCode()
@@ -674,9 +689,8 @@ try {
         \nyos\Msg::sendTelegramm($text, null, 1);
 
     die(str_replace('{text}', $text1, file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/template/body_error.htm')));
-} catch (\PDOException $ex) {
+} catch (\Exception $ex) {
 
-    
     $text1 = '<pre>--- ' . __FILE__ . ' ' . __LINE__ . '-------'
             . PHP_EOL . $ex->getMessage() . ' #' . $ex->getCode()
             . PHP_EOL . $ex->getFile() . ' #' . $ex->getLine()
